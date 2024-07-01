c3fa52827a08217941ed4861149fe7f48f8d353a
Harshdeep Mishra <harshdeepmishra82@gmail.com>
2024-07-01 19:43:53 -0400

    Added path to brave browser

--- pay.py
+++ pay.py
@@ -1,17 +1,24 @@
 from selenium import webdriver
+import os
+
+from dotenv import load_dotenv
 from pathlib import Path
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+
+debug_mode = True
+load_dotenv()
 
 script_path = Path(__file__).resolve().parent
 driver_path = script_path.joinpath("chromedriver-win64", "chromedriver.exe")
+brave_path = os.getenv("BRAVE_PATH")
 
-# print(script_path)
-print(driver_path)
-# print(type(script_path))
+chrome_options = Options()
+chrome_options.binary_location = brave_path
 
 service = Service(driver_path)
 
-driver = webdriver.Chrome(service=service)
+driver = webdriver.Chrome(service=service, options=chrome_options)
 driver.get("https://www.google.com")
 
 driver.quit()
